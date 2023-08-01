6647b13bf69151df8ff934c9407eafc67d68c00a
angeld <kim.bokholm@gmail.com>
2023-08-01 02:57:50 +0200

    Add 'search-db' Script class

--- scripts/sqlite/search-db.py
+++ scripts/sqlite/search-db.py
@@ -1,3 +1,4 @@
+from Script import Script
 import core.sqlite_helper as db
 import csv
 import os
@@ -10,69 +11,16 @@ search database
 --script search-db --script-args db=<file.db> table=<table_name>
 """
 
-class init:
+class init(Script):
 	def __init__(self):
-		self.name = '.'.join(os.path.basename(__file__).split(".")[:-1])
+		super().__init__()
+		self._path = __file__
+		self.name = '.'.join(os.path.basename(self._path).split(".")[:-1])
 		self.author = "Kim Bokholm"
 		self.description = "Search database"
-		self.requirements = ["sqlite"]
+		self.requirements.append("sqlite")
 		# ^-- maybe switch to 'dependencies' instead of 'requirements'
-		self._categories = ["sqlite"]
-		self._extend = {}
-		self._set_categories()
-		self._script_repo_dir = None
-
-	@property
-	def categories(self):
-		return self._categories
-
-	def extend(self, data={}):
-		key_list = list(data.keys())
-		if len(key_list) == 0:
-			return
-		for key in key_list:
-			self._extend[key] = data[key]
-
-	def _set_categories(self):
-		parent_dir_path = os.path.dirname(os.path.realpath(__file__))
-		parent_dir_name = os.path.basename(os.path.realpath(parent_dir_path))
-		if parent_dir_name == "scripts":
-			self._categories.append("generic")
-		else:
-			if "/scripts/" in parent_dir_path:
-				dir_index = parent_dir_path.rindex("/scripts/")
-				scripts_root_dir = parent_dir_path[dir_index+1:]
-				dir_names = scripts_root_dir.split("/")[1:]
-				for dir_name in dir_names:
-					if dir_name not in self._categories:
-						self._categories.append(dir_name)
-			elif parent_dir_name not in self._categories:
-				self._categories.append(parent_dir_name)
-
-	def _script_internals(self):
-		data = ""
-		if "_internal.script" in self._extend:
-			data = "[INTERNALS]"
-			int_table = []
-			for key in self._extend["_internal.script"]:
-				int_table.append(["  ", "%s" % key, ":", "%s" % self._extend["_internal.script"][key]])
-			
-			data += "\n%s" % tabulate(int_table, tablefmt='plain')
-			if "_internal.script.args" in self._extend:
-				script_args = self._extend["_internal.script.args"]
-				if self.name in script_args and "args" in script_args[self.name]:
-					args = script_args[self.name]["args"]
-					data += "\n\n  * Options"
-					if len(list(args)) > 0:
-						int_table = []
-						for key in args:
-							int_table.append(["  ", "%s" % key, ":", "%s" % args[key]["value"]])
-						data += "\n%s" % tabulate(int_table, tablefmt='plain')
-				else:
-					data += "\n%s" % script_args
-		else:
-			data += "\n%s" % "[!] error: extention not found - '%s'" % "_internal.script"
-		return data
+		self._categories.append("sqlite")
 
 	def _on_help(self):
 		# max 93 in length until new line (\n)
@@ -128,31 +76,6 @@ class init:
 		help_output += "\n%s" % "   --script-args table=hash_map column=comment find='jsonl|virustotal' match=multi,reverse"
 		return help_output
 	
-	def help(self):
-		verbose_mode = False
-		if "_internal.verbose_mode" in self._extend:
-			verbose_mode = self._extend["_internal.verbose_mode"]
-
-		help_output = self.name
-		if verbose_mode:
-			help_output += "\n%s" % ("-"*88)
-			help_output += "\n%s" % self._script_internals()
-			help_output += "\n%s" % ("-"*88)
-		
-		help_output += "\n  %s" % self.description
-		# max 93 in length until new line (\n)
-		# ex.:         "\n --------------------------------------------------------------------------------------------
-		help_output += "\n\n"
-		help_output += self._on_help()
-		print(help_output)
-
-	def run(self, args={}):
-		if "sqlite.db_conn" not in self._extend:
-			print("[!] error: %s; missing 'sqlite.db_conn' extention" % self.name)
-			return
-
-		self._on_run(args)
-
 	def _on_run(self, args):
 		verbose_mode = False
 		if "_internal.verbose_mode" in self._extend:
