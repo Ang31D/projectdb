f915a6bc99f444c5303e638ceaa29a256bd3654a
angeld <kim.bokholm@gmail.com>
2023-08-04 01:12:48 +0200

    Update 'del-record' Script class

--- scripts/sqlite/del-record.py
+++ scripts/sqlite/del-record.py
@@ -24,19 +24,24 @@ class init(Script):
 		help_output += "\n    table='<table_name>'    - table the record exists in"
 		help_output += "\n    column='<column_name>'  - column to match on"
 		help_output += "\n    match-on='<value>'      - value to match on"
+		help_output += "\n"
 		help_output += "\n    db-name='<db_name>'     - overrides default (\"main\") db which the table exists in"
 		help_output += "\n    options='dry-mode'      - (optional) do not commit to database, good for verification"
 		return help_output
 
 	def _on_run(self, args):
+		db_conn = self._extend["sqlite.db_conn"]
+		if db_conn is None:
+			return
+
 		if 'table' not in args:
-			print("[!] error: %s; missing 'table' option" % self.name)
+			print("[!] error: %s; missing required 'table' option" % self.name)
 			return
 		if 'column' not in args:
-			print("[!] error: %s; missing 'column' option" % self.name)
+			print("[!] error: %s; missing required 'column' option" % self.name)
 			return
 		if 'match-on' not in args:
-			print("[!] error: %s; missing 'match-on' option" % self.name)
+			print("[!] error: %s; missing required 'match-on' option" % self.name)
 			return
 
 		dry_mode = False
@@ -44,7 +49,6 @@ class init(Script):
 			dry_mode = True
 
 		rows = self._select_record(args)
-		#self._show_record(rows)
 		if rows is None:
 			print("Query failed")
 			return
@@ -60,16 +64,6 @@ class init(Script):
 			return
 
 		self._del_record(args)
-
-	def _connect_db(self, db_file):
-		if not db.db_exists(db_file):
-			print("[!] error: %s; database failed to connect - missing file '%s'" % (self.name, db_file))
-			return None
-		db_conn = db.connect(db_file)
-		if db_conn is None:
-			print("[!] error: %s; database failed to connect - '%s'" % (self.name, db_file))
-			return None
-		return db_conn
 
 	def _del_record(self, args):
 		db_conn = self._extend["sqlite.db_conn"]
@@ -114,53 +108,3 @@ class init(Script):
 		query = '''DELETE FROM %s WHERE %s = "%s"
 		''' % (table, column, match_on)
 		return query
-
-	def _show_record(self, rows):
-		for row in rows:
-			print(row)
-
-	def _drop_table(self, args):
-		verbose_mode = False
-		if "_internal.verbose_mode" in self._extend:
-			verbose_mode = self._extend["_internal.verbose_mode"]
-
-		table_name = args['table']['value']
-
-		#db_name = None
-		db_name = "main" # default database
-		if 'db-name' in args:
-			db_name = args['db-name']['value']
-
-		dry_mode = False
-		if 'mode' in args and "dry" == args['mode']['value']:
-			dry_mode = True
-
-		#db_file = args['db']['value']
-		#db_conn = self._connect_db(db_file)
-		db_conn = self._extend["sqlite.db_conn"]
-		if db_conn is None:
-			return
-	
-		if dry_mode:
-			out_drop_statement = "'%s'" % table_name
-			if db_name is not None:
-				out_drop_statement = "'%s' from db('%s')" % (table_name, db_name)
-			print("[*] %s; drop table %s" % (self.name, out_drop_statement))
-			if not db.table_exists_in_db(db_conn, db_name, table_name):
-				print("[!] %s; dry mode warning: table not found - '%s'" % (self.name, table_name))
-			else:
-				print("[!] %s; dry mode - would have dropped %s" % (self.name, out_drop_statement))
-			db.close(db_conn)
-			return
-
-		if db_name is not None:
-			if not db.drop_table_from_database(db_conn, db_name, table_name):
-				print("[!] error: %s; db_drop_table_from_database(): status: failed - '%s.%s'" % (self.name, db_name, table_name))
-				return
-			print("[*] table dropped - '%s' from db('%s')" % (table_name, db_name))
-		else:
-			if not db.drop_table(db_conn, table_name):
-				print("[!] error: %s; status: failed - '%s'" % (self.name, table_name))
-			else:
-				print("[*] table dropped - '%s'" % table_name)
-		db.close(db_conn)
