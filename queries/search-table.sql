ae33a9c23e4c658cd5962fd4fbb2f900ab5ca74f
angeld <kim.bokholm@gmail.com>
2023-08-04 01:28:51 +0200

    Add 'search-table' sql-template

---
+++ queries/search-table.sql
@@ -0,0 +1,21 @@
+Search in <table> under <column> for "keyword"
+
+    returns all matching records
+<#
+	.name: basic-search-db
+#
+	.PARAMETER: table
+	 table name to search in
+	.PARAMETER: column
+	 column name to search under
+	.PARAMETER: keyword
+	 phrase to search for in the column
+#	.PARAMETER: or_keyword
+#	 phrase to search for in the 'comment' column
+#>
+
+SELECT
+	t.*
+FROM {{table}} t
+WHERE
+	t.{{column}} LIKE "%{{keyword}}%"
