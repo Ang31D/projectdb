7f3d21954baa6ecda6879e0370b5769f345f1edb
angeld <kim.bokholm@gmail.com>
2023-09-05 00:06:02 +0200

    Add 'update-record' script

---
+++ scripts/sqlite/update-record.py
@@ -0,0 +1,119 @@
+from Script import Script
+import core.sqlite_helper as db
+import csv
+import os
+from tabulate import tabulate
+from itertools import repeat
+
+class init(Script):
+	def __init__(self):
+		super().__init__()
+		self._path = __file__
+		self.name = '.'.join(os.path.basename(self._path).split(".")[:-1])
+		self.author = "Kim Bokholm"
+		self.description = "Update database record(s)"
+		self.requirements.append("sqlite")
+		# ^-- maybe switch to 'dependencies' instead of 'requirements'
+		self._categories.append("sqlite")
+
+	def _on_help(self):
+		help_output  = "  Update record in a database table"
+		help_output += "\n  Use '--script-args' to pass options to this script."
+		help_output += "\n"
+		help_output += "\n  * Options:"
+		help_output += "\n    table='<table_name>'    - table the record exists in"
+		help_output += "\n    column='<column_name>'  - column to match on"
+		help_output += "\n    match-on='<value>'      - value to match on"
+		help_output += "\n    update='<column_name>'  - column to update on"
+		help_output += "\n    value='<value>'         - value to update to"
+		help_output += "\n"
+		help_output += "\n    db-name='<db_name>'     - overrides default (\"main\") db which the table exists in"
+		help_output += "\n    dry-mode                - (optional) do not commit to database, good for verification"
+		return help_output
+
+	def _on_run(self, args):
+		db_conn = self._extend["sqlite.db_conn"]
+		if db_conn is None:
+			return
+
+		if 'table' not in args:
+			print("[!] error: %s; missing required 'table' option" % self.name)
+			return
+		if 'column' not in args:
+			print("[!] error: %s; missing required 'column' option" % self.name)
+			return
+		if 'match-on' not in args:
+			print("[!] error: %s; missing required 'match-on' option" % self.name)
+			return
+		if 'update' not in args:
+			print("[!] error: %s; missing required 'update' option" % self.name)
+			return
+		if 'value' not in args:
+			print("[!] error: %s; missing required 'value' option" % self.name)
+			return
+
+		dry_mode = "dry-mode" in args
+
+		rows = self._select_record(args)
+		if rows is None:
+			print("Query failed")
+			return
+		if len(rows) == 0:
+			print("no records found")
+			return
+		
+		for row in rows:
+			row = [*row,]
+			print(row)
+
+		if dry_mode:
+			print("[!] dry-mode - no records updated")
+			return
+
+		self._update_record(args)
+
+	def _update_record(self, args):
+		db_conn = self._extend["sqlite.db_conn"]
+		if db_conn is None:
+			return
+		
+		query = self._build_update_query(args)
+
+		try:
+			cur = db_conn.cursor()
+			cur.execute(query, ())
+			db_conn.commit()
+			return True
+		except db.Exception.OperationalError:
+			return False
+	def _build_update_query(self, args):
+		table = args["table"]["value"]
+		column = args["column"]["value"]
+		match_on = args["match-on"]["value"]
+		update_column = args["update"]["value"]
+		new_value = args["value"]["value"]
+
+		query = '''UPDATE %s SET %s = "%s" WHERE %s = "%s"
+		''' % (table, update_column, new_value, column, match_on)
+		return query
+	
+	def _select_record(self, args):
+		db_conn = self._extend["sqlite.db_conn"]
+		if db_conn is None:
+			return None
+
+		query = self._build_select_query(args)
+		try:
+			rows = db.query_db(db_conn, query)
+			return rows
+		except db.Exception.OperationalError:
+			return []
+	def _build_select_query(self, args):
+		table = args["table"]["value"]
+		column = args["column"]["value"]
+		match_on = args["match-on"]["value"]
+
+		query = '''SELECT * FROM %s WHERE %s = "%s"
+		''' % (table, column, match_on)
+		return query
+	
